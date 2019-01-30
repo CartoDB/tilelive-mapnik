40a8dd085f1bfd2b9bd6e49a5df5f43c6162e5dd
Daniel García Aubert <danielgarciaaubert@gmail.com>
2019-01-30 10:09:20 +0100

    Add test

---
+++ test/pool.render.test.js
@@ -0,0 +1,73 @@
+const fs = require('fs');
+const assert = require('./support/assert');
+const MapnikBackend = require('..');
+const util = require('util');
+
+describe('Pool Render ', function() {
+    const tileCoords = [
+        [0, 0, 0],
+        [1, 0, 0],
+        [1, 0, 1],
+        [1, 1, 0],
+        [1, 1, 1],
+        [2, 0, 0],
+        [2, 0, 1],
+        [2, 0, 2],
+        [2, 0, 3],
+        [2, 1, 0],
+        [2, 1, 1],
+        [2, 1, 2],
+        [2, 1, 3],
+        [2, 2, 0],
+        [2, 2, 1],
+        [2, 2, 2],
+        [2, 2, 3],
+        [2, 3, 0],
+        [2, 3, 1],
+        [2, 3, 2],
+        [2, 3, 3]
+    ];
+
+    describe('getTile()', function() {
+        let source;
+
+        before(function(done) {
+            const xml = fs.readFileSync('./test/data/world.xml', 'utf8');
+            const uri = {
+                protocol: 'mapnik:',
+                pathname: './test/data/world.xml',
+                metatile: 4,
+                xml,
+                query: {
+                    poolSize: 1,
+                    poolMaxWaitingClients: 1
+                }
+            };
+
+            new MapnikBackend(uri, (err, _source) => {
+                if (err) {
+                    return done(err);
+                }
+
+                source = _source;
+                done();
+            });
+        });
+
+        it('validate: max waitingClients count exceeded', function(done) {
+            let results = [];
+
+            tileCoords.forEach(([ z, x, y ]) => {
+                source.getTile(z, x, y, (err, tile) => {
+                    results.push(err ? err : tile);
+
+                    if (results.length === tileCoords.length) {
+                        const errs = results.filter((err) => err.message === 'max waitingClients count exceeded')
+                        assert.ok(errs.length > 0);
+                        done();
+                    }
+                });
+            });
+        });
+    });
+});
