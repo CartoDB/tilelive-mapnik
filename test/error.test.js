194806841783c39e19681460c1965c09b72f6d44
Daniel García Aubert <danielgarciaaubert@gmail.com>
2019-03-17 14:43:27 +0100

    Get format from xml to compose metatile key cache

--- test/error.test.js
+++ test/error.test.js
@@ -6,32 +6,26 @@ var util = require('util');
 describe('Handling Errors ', function() {
 
     it('invalid style', function(done) {
-        new mapnik_backend({ xml: fs.readFileSync('./test/data/invalid_style.xml', 'utf8') }, function (err, source) {
-            assert.ifError(err);
-            source.getTile(0, 0, 0, (err) => {
-                assert.ok(err);
-                // first message is from rapidxml, second is from libxml2
-                assert.ok((err.message.search('expected < at line 1') !== -1) || (err.message.search('XML document not') !== -1));
-                done();
-            });
+        new mapnik_backend({ xml: fs.readFileSync('./test/data/invalid_style.xml', 'utf8'), base: './test/data/' }, function (err, source) {
+            assert.ok(err);
+            // first message is from rapidxml, second is from libxml2
+            assert.ok((err.message.search('expected < at line 1') !== -1) || (err.message.search('XML document not') !== -1));
+            done();
         });
     });
 
     // See https://github.com/mapbox/tilelive-mapnik/pull/74
     it('invalid font, strict', function(done) {
-        new mapnik_backend({ xml: fs.readFileSync('./test/data/invalid_font_face.xml', 'utf8'), strict: true }, function (err, source) {
-            assert.ifError(err);
-            source.getTile(0, 0, 0, (err) => {
-                assert.ok(err);
-                assert.ok(err.message.search("font face") !== -1, err.message);
-                done();
-            });
+        new mapnik_backend({ xml: fs.readFileSync('./test/data/invalid_font_face.xml', 'utf8'), base: './test/data/', strict: true }, function (err, source) {
+            assert.ok(err);
+            assert.ok(err.message.search("font face") !== -1, err.message);
+            done();
         });
     });
 
     // See https://github.com/mapbox/tilelive-mapnik/pull/74
     it('invalid font, non-strict (default)', function(done) {
-        new mapnik_backend({ xml: fs.readFileSync('./test/data/invalid_font_face.xml', 'utf8') }, function(err, source) {
+        new mapnik_backend({ xml: fs.readFileSync('./test/data/invalid_font_face.xml', 'utf8'), base: './test/data/' }, function(err, source) {
             assert.ok(!err, err);
             source.close(done);
         });
@@ -46,13 +40,10 @@ describe('Handling Errors ', function() {
     });
 
     it('bad style', function(done) {
-        new mapnik_backend({ xml: fs.readFileSync('./test/data/world_bad.xml', 'utf8') }, function (err, source) {
-            assert.ifError(err);
-            source.getTile(0, 0, 0, (err) => {
-                assert.ok(err);
-                assert.ok((err.message.search('invalid closing tag') !== -1) || (err.message.search('XML document not well formed') !== -1));
-                done();
-            });
+        new mapnik_backend({ xml: fs.readFileSync('./test/data/world_bad.xml', 'utf8'), base: './test/data/' }, function (err, source) {
+            assert.ok(err);
+            assert.ok((err.message.search('invalid closing tag') !== -1) || (err.message.search('XML document not well formed') !== -1));
+            done();
         });
     });
 
@@ -81,7 +72,7 @@ describe('Handling Errors ', function() {
     ['getTile', 'getGrid'].forEach(function(method) {
 
         it('coordinates out of range: ' + method, function(done) {
-            new mapnik_backend({ xml: fs.readFileSync('./test/data/test.xml', 'utf8') }, function(err, source) {
+            new mapnik_backend({ xml: fs.readFileSync('./test/data/test.xml', 'utf8'), base: './test/data/' }, function(err, source) {
                 if (err) throw err;
                 source[method](0, -1, 0, function(err) {
                     assert(err.message.match(/Coordinates out of range/), 'error message mismatch: ' + err.message);
@@ -91,7 +82,7 @@ describe('Handling Errors ', function() {
         });
 
         it('coordinates out of range, not finite: ' + method, function(done) {
-            new mapnik_backend({ xml: fs.readFileSync('./test/data/test.xml', 'utf8') }, function(err, source) {
+            new mapnik_backend({ xml: fs.readFileSync('./test/data/test.xml', 'utf8'), base: './test/data/' }, function(err, source) {
                 if (err) throw err;
                 source[method](1024, 0, 0, function(err) {
                     assert(err.message.match(/Coordinates out of range/), 'error message mismatch: ' + err.message);
--- test/pool.render.test.js
+++ test/pool.render.test.js
@@ -32,11 +32,11 @@ describe('Pool Render ', function() {
         let source;
 
         before(function(done) {
-            const xml = fs.readFileSync('./test/data/world.xml', 'utf8');
             const uri = {
                 protocol: 'mapnik:',
                 metatile: 4,
-                xml,
+                xml: fs.readFileSync('./test/data/world.xml', 'utf8'),
+                base: './test/data/',
                 query: {
                     poolSize: 1,
                     poolMaxWaitingClients: 1
